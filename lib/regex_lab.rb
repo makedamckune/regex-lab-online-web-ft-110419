703444332c6d615b29157466cb05ad7a7454dd32
Makeda P Mckune <makedamckune@gmail.com>
2019-11-10 19:16:34 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,5 +1,7 @@
 def starts_with_a_vowel?(word)
-
+ words = "At, Evolve, Everyday, Shame, Introduce"
+ 
+ word_breakdown = words.scan(/[aeiou]/)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
