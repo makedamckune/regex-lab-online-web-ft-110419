af63b6a26e6755dd6c877d9ab2bd02369ad57812
Makeda P Mckune <makedamckune@gmail.com>
2019-11-10 23:02:49 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,6 +1,6 @@
 def starts_with_a_vowel?(word)
-
-end
+ string = " afoot Excellent incredible Absolute unreal Inconceivable octopus"
+ p string.to_letter(:scan, /b[AaEeIiOoUu]/).map 
 
 def words_starting_with_un_and_ending_with_ing(text)
 
